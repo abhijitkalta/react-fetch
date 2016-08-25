ae98d2a8276b957d7558b0dc303e3abb1a7470e0
abhijit kalta <abhijitkalta@gmail.com>
2016-08-25 20:56:04 +0530

    fetch functionality added

---
+++ app/components/List.js
@@ -0,0 +1,26 @@
+var React = require('react')
+var ListItem = require('./ListItem')
+var HTTP = require('../services/httpservice');
+
+var List = React.createClass({
+  getInitialState: function(){
+    return {ingredients: []};
+  },
+
+  componentWillMount: function(){
+    HTTP.get('/ingredients')
+    .then(function(data){
+      this.setState({ingredients: data});
+    }.bind(this));
+  },
+
+  render: function() {
+    var listItems = this.state.ingredients.map(function(item){
+      return <ListItem key={item.id} text={item.text} />  ;
+    });
+
+    return (<ul>{listItems}</ul>) ;
+  }
+})
+
+module.exports = List ;
